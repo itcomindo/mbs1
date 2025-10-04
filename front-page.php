065ea3eb9065a4fb35a571c89f0bb5924272ea59
Budi Haryono <mail.budiharyono@gmail.com>
2025-10-04 10:50:13 +0700

    add part-about

--- front-page.php
+++ front-page.php
@@ -10,5 +10,6 @@ defined('ABSPATH') || die('No script kiddies please!');
 get_header();
 
 get_template_part('template-parts/part', 'hero');
+get_template_part('template-parts/part', 'about-us');
 
 get_footer();
---
+++ template-parts/part-about-us.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ *
+ * Silence is golden
+ */
+
+defined('ABSPATH') || die('No script kiddies please!');
+?>
+<section id="part-about" class="section high">
+    <div class="inner-section">
+        <div class="container">
+            <h2 class="head medium">About Us</h2>
+            <p>At Mitrabuild, Our vision is to be the provider of high-quality construction product and services in the industry. We strive to achieve this by delivering exceptional results that exceed our clients' expectations.</p>
+            <p>Our mission is to build long-lasting relationships with our clients by providing them with reliable, efficient, and cost-effective solutions for their needs.</p>
+            <p>We offer turnkey solutions, which comprises all aspects of a project from inception to completion. Our scope are design, supply, installation, comissioning, testing and inspection. We ensure a seamless and hassle-free project exectuion process for the client.</p>
+
+        </div>
+    </div>
+</section>
